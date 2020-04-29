b9963ab1567f61a6a79e4a96a2ca1969bfe8be17
aj132608 <32584157+aj132608@users.noreply.github.com>
2020-04-29 12:32:28 -0500

    Changed recipient to all when car is turned off.

--- tests/subscriber_test.py
+++ tests/subscriber_test.py
@@ -2,7 +2,7 @@ from queuingutils.subscriber import Subscriber
 
 if __name__ == "__main__":
     project_id = 'pub-sub132608'
-    subscription_name = 'sub_asdfjfdklsjdjdyebv_app'
+    subscription_name = 'sub_app'
     topic_name = 'api-pub'
 
     subscriber = Subscriber(project_id, subscription_name, topic_name)
